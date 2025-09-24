6c62c969173fde75a3e752f61221012c4733aff9
Kaveen <gihantha01@gmail.com>
2025-09-24 13:35:59 +0530

    feat: saving users api

--- user-service/index.js
+++ user-service/index.js
@@ -2,9 +2,35 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost:27017/users').then(() => {
+    console.log('Connected to MongoDB')
+}).catch(err => {
+    console.error('Could not connect to MongoDB', err)
+});
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String
+});
+
+const User = mongoose.model('User', UserSchema);
+
+app.post('/users', async (req, res) => {
+    const { name, email } = req.body;
+
+    try {
+        const user = new User({ name, email });
+        await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        console.log("Error creating user:", error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
